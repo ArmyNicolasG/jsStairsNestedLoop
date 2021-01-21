430e70f7088cd254dafadcbb3ae25a3f67f0a05a
Your Name <you@example.com>
2021-01-21 08:01:30 -0500

    1.0

---
+++ main.js
@@ -0,0 +1,14 @@
+
+let stairsQuantity = [
+     [1],
+     [1, 2],
+     [1, 2, 3],
+     [1, 2, 3, 4],
+     [1, 2, 3, 4, 5],
+]
+for(let i = 0; i <= stairsQuantity.length; i++){
+  for(let j = 0; j <= stairsQuantity[i].length; j++){
+     document.write("e")
+  }
+  document.write(`<br>`)
+}
